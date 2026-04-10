6c1a34359213e424112b1d4519df820b1bb67f86
WinnerBishal <winnerbishal9848@gmail.com>
2026-04-10 17:29:02 -0500

    added kinova_leader

--- src/lerobot/scripts/lerobot_record.py
+++ src/lerobot/scripts/lerobot_record.py
@@ -111,6 +111,7 @@ from lerobot.teleoperators import (  # noqa: F401
     so100_leader,
     so101_leader,
     xbox,
+    kinova_leader,
 )
 from lerobot.teleoperators.keyboard.teleop_keyboard import KeyboardTeleop
 from lerobot.utils.constants import ACTION, OBS_STR
